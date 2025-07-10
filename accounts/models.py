ced0bbbfb07aafc7e2057ab7958c4b0e3c95c2b6
Rob Kazirut <robert.kazirut@gmail.com>
2025-07-10 18:07:28 -0400

    account app separation of concerns

--- accounts/models.py
+++ accounts/models.py
@@ -2,27 +2,20 @@ import uuid
 
 from django.db import models
 
-from accounts.models import Account
 
-
-class Campaign(models.Model):
+class Account(models.Model):
     index = models.AutoField(
         null=False,
         primary_key=True,
     )
     uuid = models.UUIDField(null=False, default=uuid.uuid4, editable=False, unique=True)
-    name = models.CharField(null=False, max_length=100)
-    account_uuid = models.ForeignKey(
-        Account, on_delete=models.CASCADE, to_field="uuid", db_column="account_uuid"
-    )
-    start_date = models.DateField(null=True, blank=True)
-    end_date = models.DateField(null=True, blank=True)
+    username = models.CharField(null=False, max_length=100)
     last_updated = models.DateTimeField(null=False, auto_now=True)
     created_date = models.DateTimeField(null=False, auto_now_add=True)
     deleted = models.BooleanField(null=False, default=False)
 
     class Meta:
-        db_table = "campaign"
+        db_table = "account"
 
     def __str__(self):
-        return self.name
+        return self.username
