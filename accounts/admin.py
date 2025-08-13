13fc429414ee46c391cd75e4edb42678706ea039
slandath <jesus_slanda@yahoo.com>
2025-08-13 19:41:02 -0400

    added admin.py file for django admin dashboard.
    Signed-off-by: slandath <jesus_slanda@yahoo.com>

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,1 +1,51 @@
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import Account
+
+
+class AccountAdmin(UserAdmin):
+    model = Account
+    list_display = ("email", "username", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_active", "deleted")
+    ordering = ("email",)
+    search_fields = ("email", "username")
+
+    fieldsets = (
+        (None, {"fields": ("email", "username", "password")}),
+        ("Personal Info", {"fields": ("uuid",)}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_staff",
+                    "is_active",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important Dates", {"fields": ("last_login", "created_date", "last_updated")}),
+        ("Status", {"fields": ("deleted",)}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "username",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                ),
+            },
+        ),
+    )
+
+
+admin.site.register(Account, AccountAdmin)  # Register your models here.
