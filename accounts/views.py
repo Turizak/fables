e50548837556a9332f6fb8e0c791af0316f5719c
slandath <jesus_slanda@yahoo.com>
2025-08-20 11:21:19 -0400

    created form, view, template and routing.
    Signed-off-by: slandath <jesus_slanda@yahoo.com>

--- accounts/views.py
+++ accounts/views.py
@@ -1,1 +1,14 @@
-# Create your views here.
+from django.shortcuts import render
+
+from .forms import CreateAccountForm
+
+
+def create_account(request):
+    if request.method == "POST":
+        form = CreateAccountForm(request.POST)
+        if form.is_valid():
+            pass
+    else:
+        form = CreateAccountForm()
+
+    return render(request, "create_account.html", {"form": form})
